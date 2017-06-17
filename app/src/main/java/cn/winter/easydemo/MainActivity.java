8ff5e86a491d8de4bb127ee106af89668030402d
winter_java <winter_java@163.com>
2017-06-17 11:23:18 +0800

    测试2

--- app/src/main/java/cn/winter/easydemo/MainActivity.java
+++ app/src/main/java/cn/winter/easydemo/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1008611,1102
+        //1008611,5555
     }
 }
